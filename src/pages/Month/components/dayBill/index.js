aad696ad07f724c089fc57b4075cdc0e6bc47aec
zhuyulong <02029325@tb.com>
2025-06-10 11:22:29 +0800

    每日账单

--- src/pages/Month/components/dayBill/index.js
+++ src/pages/Month/components/dayBill/index.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
 import {useMemo} from 'react'
+import {billTypeToName} from '@/contents/index'
 import './index.scss'
 const DailyBill = ({date,billList}) => {
     const dayResult = useMemo(() => {
@@ -23,7 +24,7 @@ const DailyBill = ({date,billList}) => {
       <div className="header">
         <div className="dateIcon">
           <span className="date">{date}</span>
-          <span className={classNames('arrow')}></span>
+          <span className={'arrow'}></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -39,6 +40,20 @@ const DailyBill = ({date,billList}) => {
             <span className="type">结余</span>
           </div>
         </div>
+      </div>
+      <div className="billList">
+        {billList.map(item => {
+          return (
+            <div className="bill" key={item.id}>
+              <div className="detail">
+                <div className="billType">{billTypeToName[item.useFor]}</div>
+              </div>
+              <div className={classNames('money', item.type)}>
+                {item.money.toFixed(2)}
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
